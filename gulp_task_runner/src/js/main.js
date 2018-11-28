da72c64a7b8d2b35795edaf6a62f68b57c085de0
osisamkay <osisami.oj@gmail.com>
2018-11-28 15:01:37 +0100

    working with gulp task runner

---
+++ gulp_task_runner/src/js/main.js
@@ -0,0 +1,34 @@
+(function(){
+  const host = 'http://api.giphy.com/v1';
+  const apiKey = 'OB060zxrMKSeSARlNX33j6ohNTm3DUED&limit=12';
+  const form = document.getElementById('form');
+  const input = document.getElementById('searchInput');
+  const output = document.getElementById('output');
+  const images = {
+    img0: document.getElementById('img1'),
+    img1: document.getElementById('img2'),
+    img2: document.getElementById('img3'),
+  }
+  form.addEventListener('submit', event => {
+    event.preventDefault();
+    const gif=document.createElement('img')
+    gif.src='../../assets/load.gif'
+    output.appendChild(gif);
+
+    axios.get(`${host}/gifs/search?q=${input.value}&api_key=${apiKey}&limit=3`)
+    .then(function (response) {
+      // handle success
+      output.textContent = '';
+      // response.data.data
+      const { data: { data } } = response;
+      data.forEach((gif, index) => {
+        images[`img${index}`].src = gif.images.original.url;
+      });
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+      output.textContent = error;
+    })
+  });
+}());

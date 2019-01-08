d09ca0289431850d25d6d86cdb7e327b7e195c68
osisamkay <osisami.oj@gmail.com>
2019-01-08 19:48:23 +0100

    updated

--- portfolio2/dist/js/app.js
+++
@@ -1,2 +0,0 @@
-"use strict";!function(){var e=document.querySelector(".icon"),t=document.querySelector(".nav-hide");e.addEventListener("click",function(){t.classList.toggle("mobile")});var n=document.querySelector("#date"),c=(new Date).getFullYear();n.textContent=c}();
-//# sourceMappingURL=maps/app.js.map
